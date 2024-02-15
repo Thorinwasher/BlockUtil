1b64a4260e14a10130e7e6b7b764438a803e6063
Thorinwasher <ragnarlol96@gmail.com>
2024-02-15 14:23:43 +0100

    Remove unused code

--- src/main/java/dev/thorinwasher/blockutil/util/BlockHelper.java
+++ src/main/java/dev/thorinwasher/blockutil/util/BlockHelper.java
@@ -18,10 +18,6 @@ import java.util.List;
 import java.util.Set;
 
 public class BlockHelper {
-
-    private static final Set<Material> NEEDS_BLOCK_BELOW = getNeedsBlockBelow();
-
-    private static final Set<Material> NEEDS_SUPPORT = getNeedsSupport();
     private BlockHelper() {
         throw new IllegalStateException("Utility class");
     }
@@ -78,66 +74,5 @@ public class BlockHelper {
                 block.getRelative(BlockFace.EAST),
                 block.getRelative(BlockFace.SOUTH),
                 block.getRelative(BlockFace.WEST));
-    }
-
-    public static boolean needsSupport(Material type){
-        return NEEDS_SUPPORT.contains(type);
-    }
-
-    private static Set<Material> getNeedsSupport(){
-        Set<Material> temp = new HashSet<>();
-        temp.add(Material.CHORUS_FLOWER);
-        temp.add(Material.CHORUS_PLANT);
-        temp.add(Material.SCAFFOLDING);
-        temp.addAll(Tag.CAVE_VINES.getValues());
-        temp.add(Material.WEEPING_VINES);
-        temp.add(Material.TWISTING_VINES);
-        temp.addAll(Tag.SIGNS.getValues());
-        temp.addAll(NEEDS_BLOCK_BELOW);
-        temp.addAll(Tag.WALL_CORALS.getValues());
-        temp.add(Material.DEAD_BRAIN_CORAL_WALL_FAN);
-        temp.add(Material.DEAD_BUBBLE_CORAL_WALL_FAN);
-        temp.add(Material.DEAD_FIRE_CORAL_WALL_FAN);
-        temp.add(Material.DEAD_HORN_CORAL_WALL_FAN);
-        temp.add(Material.WALL_TORCH);
-        temp.add(Material.REDSTONE_WALL_TORCH);
-        temp.add(Material.COCOA);
-        temp.add(Material.LADDER);
-        temp.add(Material.TRIPWIRE_HOOK);
-        temp.addAll(Tag.WALL_SIGNS.getValues());
-        temp.addAll(Tag.BUTTONS.getValues());
-        temp.add(Material.LEVER);
-        temp.add(Material.BELL);
-        temp.addAll(Tag.BANNERS.getValues());
-        return Set.copyOf(temp);
-    }
-
-    private static Set<Material> getNeedsBlockBelow() {
-        Set<Material> temp = new HashSet<>();
-        temp.add(Material.CAKE);
-        temp.add(Material.COMPARATOR);
-        temp.add(Material.REDSTONE_WIRE);
-        temp.add(Material.REPEATER);
-        temp.addAll(Tag.SAPLINGS.getValues());
-        temp.add(Material.SEA_PICKLE);
-        temp.addAll(Tag.STANDING_SIGNS.getValues());
-        temp.add(Material.SNOW);
-        temp.addAll(Tag.BANNERS.getValues());
-        temp.addAll(Tag.FLOWER_POTS.getValues());
-        temp.addAll(Tag.PRESSURE_PLATES.getValues());
-        temp.addAll(Tag.DOORS.getValues());
-        temp.addAll(Tag.CROPS.getValues());
-        temp.addAll(Tag.RAILS.getValues());
-        temp.addAll(Tag.TALL_FLOWERS.getValues());
-        temp.addAll(Tag.WOOL_CARPETS.getValues());
-        temp.add(Material.MOSS_CARPET);
-        temp.add(Material.TORCH);
-        temp.add(Material.REDSTONE_TORCH);
-        temp.addAll(Tag.CORAL_PLANTS.getValues());
-        temp.add(Material.DEAD_BRAIN_CORAL_FAN);
-        temp.add(Material.DEAD_BUBBLE_CORAL_FAN);
-        temp.add(Material.DEAD_FIRE_CORAL_FAN);
-        temp.add(Material.DEAD_HORN_CORAL_FAN);
-        return Set.copyOf(temp);
     }
 }
